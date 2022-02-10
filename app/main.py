f39b4144a0195b11d9eaff5bf25caa0c393b3fdc
Habibur-Rahman0927 <habibur.rahaman.0927@gmail.com>
2022-02-10 21:19:37 +0600

    updated

--- app/main.py
+++ app/main.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
-from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
 from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from sqlalchemy.orm import Session
-from . import models
+from . import models, schema, utils
 from .database import engine, get_db
 
 
@@ -18,98 +17,75 @@ app = FastAPI()
 # db
 get_db()
 
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
-
-
-# try:
-#     conn = psycopg2.connect(host="localhost", database='FastApi', user="postgres", password='asdf1234', cursor_factory=RealDictCursor)
-#     cursor = conn.cursor()
-#     print("Database connection was succesfull")
-# except Exception as error:
-#     print("connect to database failed")
-#     print("Error", error)
-
-
-# my_posts = [
-#     {"id": 1, "title": "title of post 1", "content": "content of post 1"},
-#     {"id": 2, "title": "title of post 2", "content": "content of post 2"},
-# ]
-
-# def find_post(id):
-#     for p in my_posts:
-#         if p['id'] == id:
-#             return p
-
-
-# def find_index_post(id):
-#     for i, p in enumerate(my_posts):
-#         if p['id'] == id:
-#             return i
-
 @app.get("/")
 def read_root():
     return {"Habib Rahman api is runing"}
 
 
-@app.get("/posts")
+@app.get("/posts", response_model = List[schema.PostRespone])
 def get_posts(db: Session = Depends(get_db)):
-    # cursor.execute("SELECT * FROM posts")
-    # posts = cursor.fetchall()
-    # print(posts)
     posts = db.query(models.Post).all()
-    return {"data": posts}
+    return posts
 
 
-@app.post("/posts", status_code = status.HTTP_201_CREATED)
-def createpost(post: Post, db: Session = Depends(get_db)):
-    # cursor.execute("INSERT INTO posts (title, content, published) VALUES (%s,%s,%s) RETURNING *", (post.title, post.content, post.published))
-    # posts = cursor.fetchone()
-    # conn.commit()
-    # new_posts = models.Post(title = post.title, content = post.content, published = post.published)
+@app.post("/posts", status_code = status.HTTP_201_CREATED, response_model = schema.PostRespone)
+def createpost(post: schema.Post, db: Session = Depends(get_db)):
     new_posts = models.Post(**post.dict())
     db.add(new_posts)
     db.commit()
     db.refresh(new_posts)
-    return {"data": new_posts}
+    return new_posts
 
 
-@app.get("/posts/{id}")
+@app.get("/posts/{id}", response_model = schema.PostRespone)
 def get_single_posts(id: int, response: Response, db: Session = Depends(get_db)):
-    # cursor.execute(f"SELECT * FROM posts WHERE id = {id}")
-    # post = cursor.fetchone()
     post = db.query(models.Post).filter(models.Post.id == id).first()
 
     if not post:
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"post with id: {id} was not found")
-    return {"data": post}
+    return post
 
 
 @app.delete("/posts/{id}", status_code = status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db)):
-    # cursor.execute("DELETE FROM posts WHERE id = %s RETURNING *", (str(id)))
-    # delete_post = cursor.fetchone()
-    # conn.commit()
     delete_post = db.query(models.Post).filter(models.Post.id == id)
     if delete_post.first() == None:
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"post with id: {id} was not found")
     delete_post.delete(synchronize_session=False)
     db.commit()
-    return (Response(status_code = status.HTTP_204_NO_CONTENT), {"delete_data": delete_post})
+    return (Response(status_code = status.HTTP_204_NO_CONTENT), delete_post)
 
 
-@app.put("/posts/{id}")
-def update_post(id: int, post: Post, db: Session = Depends(get_db)):
-    # cursor.execute("UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING * ", (post.title, post.content, post.published, str(id)))
-    # my_posts = cursor.fetchone()
-    # conn.commit()
+@app.put("/posts/{id}", response_model = schema.PostRespone)
+def update_post(id: int, post: schema.UpdatePost, db: Session = Depends(get_db)):
     my_post = db.query(models.Post).filter(models.Post.id == id)
     post_db = my_post.first()
     if post_db == None:
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"post with id: {id} was not found")
     my_post.update(post.dict(), synchronize_session=False)
     db.commit()
-    return {"data": my_post.first()}
+    return my_post.first()
 
+
+@app.post("/users", status_code = status.HTTP_201_CREATED, response_model = schema.UserResponse)
+def createpost(user: schema.UserCreate, response: Response, db: Session = Depends(get_db)):
+    # if user.email:
+    #     userValid = db.query(models.User).filter(models.User.email == user.email).first()
+    #     return {"msg": "User is Already Exist"}
+        
+    hashed_password = utils.hash(user.password)
+    user.password = hashed_password
+    new_user = models.User(**user.dict())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+@app.get('/users/{id}', response_model = schema.UserResponse)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    
+    if not user:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail=f"User with id: {id} does not exist")
+
+    return user
---
+++ app/schema.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+class UpdatePost(BaseModel):
+    title: str
+    content: str
+    published: bool
+
+class PostRespone(Post):
+    id: int
+    created_at: datetime
+
+    class Config:
+        orm_mode=True
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserResponse(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+    class Config:
+        orm_mode=True
